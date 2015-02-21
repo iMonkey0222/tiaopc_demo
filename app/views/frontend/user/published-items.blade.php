023894c89a29bd894bf22f7391aa9e8ae69fc90b
xiaoxiao <xiaoyang@pc-212-27.wireless.prospect.liberty.lan>
2015-02-21 14:51:42 +0000

    xiaoyang wang

--- app/views/frontend/user/published-items.blade.php
+++ app/views/frontend/user/published-items.blade.php
@@ -12,7 +12,30 @@ Published Items
 </div>
 
 <form method="post" action="" class="form-horizontal" autocomplete="off">
+	<table>
+		<thead>
+			<tr>
+				<th>Products</th>
+				<th>Prices</th>
+				<th>trade Status</th>
+			</tr>
+			<tr><td colspan="9">this is step row</td></tr>
+			<tr>
+				<td colspan="7">
+					<div class="controls">
+						<label class="control-label" for="select all">
+							<input type = "checkbox" name="select all" id="select all" value = "Select All" /> 
+						</label>
+					</div>
+				</td>
+			</tr>
+		</thead>
 
+		<tbody>
+			This is body
+			<tr><td colspan="7"></td></tr>
+		</tbody>
+	</table>
 
 
 </form>
