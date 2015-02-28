3622d13aba6faf2da94de3a84029560777605253
Patrick Wang <patrick.wang1029@gmail.com>
2015-02-28 22:01:11 +0000

    yw updata db

--- app/config/database.php
+++ app/config/database.php
@@ -54,10 +54,10 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => '178.62.4.188',
-			'database'  => 'tiaopc',
-			'username'  => 'forge',
-			'password'  => '6LchyXte9mrg2Z3hTQrf',
+			'host'      => $_ENV['DB_HOST'],
+			'database'  => $_ENV['DB_NAME'],
+			'username'  => $_ENV['DB_USERNAME'],
+			'password'  => $_ENV['DB_PASSWORD'],
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => 'laravel_',
