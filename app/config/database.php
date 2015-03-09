f58e4f344c385257ef03167418537c8636470a7b
iMonkey0222 <xiaoyang.wang222@gmail.com>
2015-03-09 17:18:22 +0000

    database

--- app/config/database.php
+++ app/config/database.php
@@ -54,23 +54,26 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-		'host'      => 'localhots',
-		'database'  => 'laravel_tiaopc_demo',
-		'username'  => 'root',
-		'password'  => 'root',
+			'host'      => $_ENV['DB_HOST'],
+			'database'  => $_ENV['DB_NAME'],
+			'username'  => $_ENV['DB_USER'],
+			'password'  => $_ENV['DB_PASSWORD'],
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => 'laravel_',
 
 		),
 
-
+		// 'host'      => 'localhots',
+		// 'database'  => 'laravel_tiaopc_demo',
+		// 'username'  => 'root',
+		// 'password'  => 'root',
 
 			// 'host'      => $_ENV['DB_HOST'],
 			// 'database'  => $_ENV['DB_NAME'],
 			// 'username'  => $_ENV['DB_USER'],
 			// 'password'  => $_ENV['DB_PASSWORD'],
-			
+
 		// 'mysql' => array(
 		// 	'driver'    => 'mysql',
 		// 	'host'      => '192.241.190.185',
