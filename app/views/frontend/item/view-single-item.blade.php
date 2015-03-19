e5cb6f2023cf760542eb921e46b0814a65cd0ac8
Patrick Wang <patrick.wang1029@gmail.com>
2015-03-19 12:34:09 +0000

    yw

--- app/views/frontend/item/view-single-item.blade.php
+++ app/views/frontend/item/view-single-item.blade.php
@@ -108,8 +108,8 @@
                                     <div class="panel-body">
                                         <span itemprop="productID" class="sku_wrapper">ID: <span class="sku">{{$item->id}}</span></span>
                                         <span class="posted_in">Category: <a href="#" rel="tag">{{$item->parent_category_name}}.{{$item->category_name}}</a>.</span>
-                                        <span>Condition: {{ $item->product_condition }}</span>
-                                        <span>Created at: {{ $item->created_at }}</span>
+                                        {{-- <span>Condition: {{ $item->product_condition }}</span> --}}
+                                        {{-- <span>Created at: {{ $item->created_at }}</span> --}}
                                     </div>
                                 </div><!-- Product Single - Meta End -->
                             </div>
