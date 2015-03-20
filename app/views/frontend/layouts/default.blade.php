34a416c21cfcaaee8595c38b1752f3e7d032eaa6
Patrick Wang <patrick.wang1029@gmail.com>
2015-03-20 13:38:43 +0000

    yw

--- app/views/frontend/layouts/default.blade.php
+++ app/views/frontend/layouts/default.blade.php
@@ -196,7 +196,7 @@
 
                     <div class="col_half">
                         <div class="copyrights-menu copyright-links clearfix">
-                            <a href="{{ route('home')}}">Home</a>/<a href="{{ route('about-us') }}">About</a>/<a href="{{ route('about-us') }} ">Feature</a>/<a href="{{ route('how-to-use') }}">How to use</a>/<a href="#">Contact</a>
+                            <a href="{{ route('home')}}">Home</a>/<a href="{{ route('about-us') }}">About</a>/<a href="{{ route('about-us') }} ">Feature</a>/<a href="{{ URL::to('how-to-use') }}">How to use</a>/<a href="#">Contact</a>
                         </div>
                         Copyrights &copy; 2015 All Rights Reserved by Tiaopc.
                     </div>
