775ddcdd67df46cd6997402d56ee4e9e41727b10
iMonkey0222 <xiaoyang.wang222@gmail.com>
2015-03-21 01:15:09 +0000

    xy w send email to buyer when seller  accept

--- app/views/emails/notify-request.blade.php
+++ app/views/emails/notify-request.blade.php
@@ -5,7 +5,7 @@
 
 <p>Thanks for using Tiaopc, one of your reuqests has been approved, please follow the link below to check the seller info and <strong>Get in Touch</strong> </p>
 
-<p><a href="{{ route('singleItem', $itemId) }}">{{ route('singleItem', $itemId) }}</a></p>
+<p><a href="{{ route('singleItem', $itemID) }}">{{ route('singleItem', $itemID) }}</a></p>
 
 <p>Best regards,</p>
 
