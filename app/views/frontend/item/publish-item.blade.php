dd2953b5cc0362aed88deae075a8fb9d69118f17
iMonkey0222 <xiaoyang.wang222@gmail.com>
2015-03-21 23:56:33 +0000

    publish title

--- app/views/frontend/item/publish-item.blade.php
+++ app/views/frontend/item/publish-item.blade.php
@@ -10,7 +10,7 @@ Publish Item ::
 <section id="page-title">
     <div class="container clearfix">
         <h3>Start Selling</h3>
-        <span>Sell an Item</span>
+        <span>Sell My Second Hand Digital Product</span>
  		<ol class="breadcrumb">
             <li class="active"><h5><a class="icon-home" href="http://tiaopc.com/">Home</a></h5></li>
         </ol>
