bda1c4ae1a619576f4c0b0767b2c48c67540b8bf
Patrick Wang <patrick.wang1029@gmail.com>
2015-03-21 11:58:30 +0000

    yw

--- app/controllers/account/ProfileController.php
+++ app/controllers/account/ProfileController.php
@@ -26,8 +26,8 @@ class ProfileController extends AuthorizedController {
 
 		// Declare the rules for the form validation
 		$rules = array(
-			'first_name'	=>'required|min:3',
-			'last_name'		=>'required|min:3',
+			'first_name'	=>'required|min:2',
+			'last_name'		=>'required|min:2',
 			'phone_number' 	=>'required|numeric|digits:11',
 			);
 		// Create a new validator instance from our validation rules
